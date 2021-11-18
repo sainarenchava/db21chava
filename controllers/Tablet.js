485f90220ff6733e475c517988245ae83c30f64f
sainarenchava <S544921@NWMISSOURI.EDU>
2021-11-17 20:26:02 -0600

    added view for delete

--- controllers/Tablet.js
+++ controllers/Tablet.js
@@ -119,3 +119,31 @@ exports.Tablet_create_Page =  function(req, res) {
         res.send(`{'error': '${err}'}`); 
     } 
 }; 
+
+// Handle building the view for updating a Tablet. 
+// query provides the id 
+exports.Tablet_update_Page =  async function(req, res) { 
+    console.log("update view for item "+req.query.id) 
+    try{ 
+        let result = await Tablet.findById(req.query.id) 
+        res.render('Tabletupdate', { title: 'Tablet Update', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+
+// Handle a delete one view with id from query 
+exports.Tablet_delete_Page = async function(req, res) { 
+    console.log("Delete view for id "  + req.query.id) 
+    try{ 
+        result = await Tablet.findById(req.query.id) 
+        res.render('Tabletdelete', { title: 'Tablet Delete', toShow: 
+result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
--- routes/Tablet.js
+++ routes/Tablet.js
@@ -11,4 +11,10 @@ router.get('/detail', Tablet_controlers.Tablet_view_one_Page);
 /* GET create Tablet page */ 
 router.get('/create', Tablet_controlers.Tablet_create_Page); 
 
+/* GET create update page */ 
+router.get('/update', Tablet_controlers.Tablet_update_Page);
+
+/* GET create Tablet page */ 
+router.get('/delete', Tablet_controlers.Tablet_delete_Page); 
+
 module.exports = router;
