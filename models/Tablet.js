4c1462f056616dbac686f3aaa8e73417b394ecad
sainarenchava <S544921@NWMISSOURI.EDU>
2021-11-30 21:26:03 -0600

    bonus

--- models/Tablet.js
+++ models/Tablet.js
@@ -1,4 +1,4 @@
 const mongoose = require("mongoose") 
-const TabletSchema = mongoose.Schema({ Tablet_type: Number, composition: String, Tablet_features: String }) 
+const TabletSchema = mongoose.Schema({ Tablet_type: {type: Number, min:1, max:100}, composition: {type: String, minlength: 2, maxlength: 10}, Tablet_features: String }) 
  
 module.exports = mongoose.model("Tablet", TabletSchema)
--- routes/Tablet.js
+++ routes/Tablet.js
@@ -19,12 +19,12 @@ router.get('/', Tablet_controlers.Tablet_view_all_Page);
 router.get('/detail', Tablet_controlers.Tablet_view_one_Page); 
 
 /* GET create Tablet page */ 
-router.get('/create', Tablet_controlers.Tablet_create_Page); 
+router.get('/create',secured, Tablet_controlers.Tablet_create_Page); 
 
 /* GET create update page */ 
 router.get('/update',secured, Tablet_controlers.Tablet_update_Page);
 
 /* GET create Tablet page */ 
-router.get('/delete', Tablet_controlers.Tablet_delete_Page); 
+router.get('/delete',secured, Tablet_controlers.Tablet_delete_Page); 
 
 module.exports = router;
