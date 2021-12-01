1dc410f543a5c5723e84ccdcc258bd080b2a2940
sainarenchava <S544921@NWMISSOURI.EDU>
2021-11-30 20:58:26 -0600

    added authentication

--- routes/Tablet.js
+++ routes/Tablet.js
@@ -2,6 +2,16 @@ var express = require('express');
 const Tablet_controlers= require('../controllers/Tablet');
 var router = express.Router();
 
+// A little function to check if we have an authorized user and continue on 
+//or 
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
 /* GET home page. */
 router.get('/', Tablet_controlers.Tablet_view_all_Page);
 
@@ -12,7 +22,7 @@ router.get('/detail', Tablet_controlers.Tablet_view_one_Page);
 router.get('/create', Tablet_controlers.Tablet_create_Page); 
 
 /* GET create update page */ 
-router.get('/update', Tablet_controlers.Tablet_update_Page);
+router.get('/update',secured, Tablet_controlers.Tablet_update_Page);
 
 /* GET create Tablet page */ 
 router.get('/delete', Tablet_controlers.Tablet_delete_Page); 
